2a1d7f636b03695aef94e5a30bd75f4cdfe4eea1
20181248 이현승 <53048916+CaptainLee-P@users.noreply.github.com>
2021-10-28 09:05:01 +0000

    HelloSDL 분리하기

--- main.cpp
+++ main.cpp
@@ -2,16 +2,13 @@
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
-
-int main(int argc, char* args[])
+bool g_bRunning =false;
+bool init(const char* title, int xpos, int ypos,int height, int width, int flags)
 {
- 
-   if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) >= 0) {
 
-        g_pWindow = SDL_CreateWindow("Setting up SDL 10/22",
-            SDL_WINDOWPOS_CENTERED,
-            SDL_WINDOWPOS_CENTERED,
-            640, 480, SDL_WINDOW_SHOWN);
+        g_pWindow = SDL_CreateWindow(title, xpos,ypos,height,width,flags
+           );
 
         if (g_pWindow != 0) {
             g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
@@ -19,15 +16,41 @@ int main(int argc, char* args[])
 
     }
     else {
-        return 1;
+        return false;
     }
-
-    SDL_SetRenderDrawColor(g_pRenderer, 255, 0, 0, 255);
+    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+    return true;
+}
+void render()
+{
     SDL_RenderClear(g_pRenderer);
     SDL_RenderPresent(g_pRenderer);
+}
+void update()
+{
+  SDL_SetRenderDrawColor(g_pRenderer,rand()%256,rand()%256,rand()%256,255);
+  SDL_Delay(1000);
+}
+int main(int argc, char* args[])
+{
+ 
+  if(init("Setting up SDL 10/22", SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            640, 480, SDL_WINDOW_SHOWN)) 
+  {
+    g_bRunning=true;
+  } 
+  else
+  {
+    return 1;
+  }
+  while(g_bRunning)
+  {
+    update();
+    render();
+  }
 
-    SDL_Delay(5000);
-    SDL_Quit();
+  SDL_Quit();
 
-    return 0;
+   return 0;
 }
